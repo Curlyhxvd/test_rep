b57877c8debb72e819c37e664ee951b0fe1f1b9a
Fedor Tashlykov <tashlykov.fedor@mail.ru>
2022-03-22 19:38:33 +0300

    verison control

---
+++ IdeaProjects/lab6/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        FractalExplorer fractalExplorer = new FractalExplorer(600);
+        fractalExplorer.setGUI();
+        fractalExplorer.drawFractal();
+    }
+}
